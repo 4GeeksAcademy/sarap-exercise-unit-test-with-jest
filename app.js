bfe5adb91e492ffcf710727ce6951a15f4bd03a3
SaraPe <135864591+sarap29@users.noreply.github.com>
2023-08-23 20:34:36 +0000

    Prueba realizada con exito

---
+++ app.js
@@ -0,0 +1,32 @@
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+const fromDollarToYen = (dolar) => {
+    return dolar * oneEuroIs ["JPY"] / oneEuroIs["USD"];
+}
+
+function fromEuroToDollar(euro) {
+    // Convert from euros to dollars using the conversion rate
+    return euro * oneEuroIs["USD"];
+}
+
+
+function fromYenToPound(yen) {
+    return yen * oneEuroIs["GBP"] / oneEuroIs["JPY"];
+}
+
+
+
+
+const sum = (a,b) => {
+    return a + b
+}
+
+console.log(sum(7,3))
+
+
+module.exports = { sum, fromEuroToDollar, fromYenToPound, fromDollarToYen }
